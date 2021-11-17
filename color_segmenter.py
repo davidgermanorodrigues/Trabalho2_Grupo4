cb38a89cc4d0a25fdba794fd33a3e23f3709e953
davidgermanorodrigues <germanomrodrigues@gmail.com>
2021-11-17 21:02:38 +0000

    #1 color_segmenter.py completo
    Boa noite pessoal,
    O color_segmenter.py já se encontra feito totalmente. Tem ver se encontram alguns bugs, e se encontrem algum tentem resolvê-lo.
    
    Cumprimentos,
    Germano Rodrigues

--- color_segmenter.py
+++ color_segmenter.py
@@ -14,7 +14,7 @@ slider_max = 255
 trackbar_pos = 0
 
 
-def onTrackbar():
+def onTrackbar(threshold):
     pass
 
 
@@ -24,11 +24,6 @@ def main():
     capture = cv2.VideoCapture(0)
 
     cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
-
-    #Dicionário com as ranges
-    ranges = {'b':{'min': 0, 'max': 40},
-              'g': {'min': 50, 'max': 256},
-              'r': {'min': 0, 'max': 40}}
 
     #Criação das trackbars
     cv2.createTrackbar('trackbar_min_r', window_name, 0, slider_max, onTrackbar)
@@ -49,27 +44,43 @@ def main():
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         #Vai buscar o valor da trackbar na imagem 'original'
-        ranges['b']['min'] = cv2.getTrackbarPos('trackbar_min_b', window_name)
-        ranges['g']['min'] = cv2.getTrackbarPos('trackbar_min_g', window_name)
-        ranges['r']['min'] = cv2.getTrackbarPos('trackbar_min_r', window_name)
-        ranges['b']['max'] = cv2.getTrackbarPos('trackbar_max_b', window_name)
-        ranges['g']['max'] = cv2.getTrackbarPos('trackbar_max_g', window_name)
-        ranges['r']['max'] = cv2.getTrackbarPos('trackbar_max_r', window_name)
+        b_min = cv2.getTrackbarPos('trackbar_min_b', window_name)
+        g_min = cv2.getTrackbarPos('trackbar_min_g', window_name)
+        r_min = cv2.getTrackbarPos('trackbar_min_r', window_name)
+        b_max = cv2.getTrackbarPos('trackbar_max_b', window_name)
+        g_max = cv2.getTrackbarPos('trackbar_max_g', window_name)
+        r_max = cv2.getTrackbarPos('trackbar_max_r', window_name)
+
+        # Dicionário com as ranges
+        ranges = {'b': {'min': b_min, 'max': b_max},
+                  'g': {'min': g_min, 'max': g_max},
+                  'r': {'min': r_min, 'max': r_max}}
 
         #Arruma tudo em numpy arrays
         mins = np.array([ranges['b']['min'], ranges['g']['min'], ranges['r']['min']])
         maxs = np.array([ranges['b']['max'], ranges['g']['max'], ranges['r']['max']])
 
         # Faz a máscara
-        mask = cv2.inRange(image_gui, mins, maxs)
+        image_processed = cv2.inRange(image_gui, mins, maxs)
 
         # Mostra a imagem
         cv2.imshow(window_name, image)
-        cv2.imshow('mask', mask)
+        cv2.imshow('mask', image_processed)
 
         key = cv2.waitKey(5)
 
-        if key == ord('q'):  # q for quit:
+        if key == ord('w'):  # guarda os valores das trackbars no ficheiro limits.json na mesma pasta do programa
+
+            data = {'limits': {'B': {'max': ranges['b']['max'], 'min': ranges['b']['min']},
+                               'G': {'max': ranges['g']['max'], 'min': ranges['g']['min']},
+                               'R': {'max': ranges['r']['max'], 'min': ranges['r']['min']}, }}
+
+            with open(file_name, 'w') as file_handle:
+                print('writing dictionary data to file ' + file_name)
+                json.dump(data, file_handle)
+
+
+        if key == ord('q'):  # sai do programa e não guarda
 
             data = {'limits': {'B': {'max': ranges['b']['max'], 'min': ranges['b']['min']},
                                'G': {'max': ranges['g']['max'], 'min': ranges['g']['min']},
@@ -80,7 +91,6 @@ def main():
                 json.dump(data, file_handle)
             print('You pressed q, aborting.')
             break
-
     cv2.destroyAllWindows()
 if __name__ == '__main__':
     main()
