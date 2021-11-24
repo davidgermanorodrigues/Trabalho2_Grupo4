603728dc166206825e423fb4210ebd17abda683c
davidgermanorodrigues <germanomrodrigues@gmail.com>
2021-11-24 00:10:14 +0000

    #4 Update ar_paint.py - Funcionalidade avançada 3 (desenhar formas na tela)

--- ar_paint.py
+++ ar_paint.py
@@ -73,6 +73,8 @@ def main():
     i = 0
     pencil_color = (0,0,0)
     pencil_thickness = 10
+    Cx = 0
+    Cy = 0
 
     _, image = capture.read()           # get an image from the camera
     height, width, _, = image.shape       # get dimensions of the image
@@ -98,6 +100,54 @@ def main():
         # Criação da máscara para isolar o objeto maior
         mask_largest = np.ndarray(shape=(height, width), dtype=np.uint8)
         mask_largest.fill(0)        #Totalmente preta
+
+        if np.mean(image_processed) > 0:  # Verifica se existem objetos (>0)
+
+            count += 1
+
+            contours, hierarchy = cv2.findContours(image_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+            largest_item = sorted_contours[0]  # Vai buscar o objeto maior
+            cv2.fillPoly(mask_largest, pts=[largest_item], color=(255, 255, 255))  # Faz o fill do objeto maior
+
+            # Calcula as coordenadas do centróide e assinala-o
+            M = cv2.moments(mask_largest)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            # cv2.circle(image, (cX, cY), 5, (0, 0, 255), 2)
+
+            # Desenhar a verde na imagem original
+            mask_largest = mask_largest.astype(np.bool)  # Temos de converter a mask_largest em bool para puder usá-la como 'filtro' na imagem original
+            image[mask_largest] = (0, 255, 0)  # Pintamos de verde na imagem original
+
+            # Desenha uma cruz vermelha no centróide
+            dim_cross = 5
+            cv2.line(image, pt1=(cX - dim_cross, cY - dim_cross), pt2=(cX + dim_cross, cY + dim_cross), color=(0, 0, 255), thickness=2)
+            cv2.line(image, pt1=(cX - dim_cross, cY + dim_cross), pt2=(cX + dim_cross, cY - dim_cross), color=(0, 0, 255), thickness=2)
+
+            # Desenha a linha na máscara branca
+            center_X.append(cX)
+            center_Y.append(cY)
+
+            # print(len(center_X))
+            # print(center_X)
+            # print(center_Y)
+
+            if args.get('shake') == True:
+                if count == 2:
+                    shake_prevention(center_X, center_Y, mask_white, pencil_color, pencil_thickness)
+
+            else:
+                # Verifica se existem argumentos suficientes nos arrays dos centróides para desenhar as linhas
+                if len(center_X) >= 2:
+                    cv2.line(mask_white, pt1=(center_X[count - 2], center_Y[count - 2]),
+                             pt2=(center_X[count - 1], center_Y[count - 1]), color=pencil_color,
+                             thickness=pencil_thickness)
+
+            if count >= 2:
+                del center_X[0]
+                del center_Y[0]
+                count -= 1
 
         key = cv2.waitKey(5)
 
@@ -139,64 +189,110 @@ def main():
             status = cv2.imwrite(time.strftime('drawing_%a_%b_%Y-%m-%d-%H:%M') + '.png', mask_white)
             print('You pressed w, canvas is now saved.')
 
+        #Desenha círculos na tela (canvas)
+        elif key == ord('o'):
+
+            cent_atual = (center_X[len(center_X)-1] , center_Y[len(center_Y)-1])
+            print(cent_atual)
+
+            while True:
+                _, image = capture.read()  # get an image from the camera
+
+                image = cv2.flip(image, 1)  # espelhar imagem da webcam
+
+                # Criação da imagem processada
+                mins = np.array([ranges['b']['min'], ranges['g']['min'], ranges['r']['min']])
+                max = np.array([ranges['b']['max'], ranges['g']['max'], ranges['r']['max']])
+                image_processed = cv2.inRange(image, mins, max)
+
+                # Criação da máscara para isolar o objeto maior
+                mask_largest = np.ndarray(shape=(height, width), dtype=np.uint8)
+                mask_largest.fill(0)  # Totalmente preta
+
+                if np.mean(image_processed) > 0:  # Verifica se existem objetos (>0)
+
+                    contours, hierarchy = cv2.findContours(image_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+                    largest_item = sorted_contours[0]  # Vai buscar o objeto maior
+                    cv2.fillPoly(mask_largest, pts=[largest_item], color=(255, 255, 255))  # Faz o fill do objeto maior
+
+                    # Calcula as coordenadas do centróide e assinala-o
+                    M = cv2.moments(mask_largest)
+                    cX1 = int(M["m10"] / M["m00"])
+                    cY1 = int(M["m01"] / M["m00"])
+                    # cv2.circle(image, (cX, cY), 5, (0, 0, 255), 2)
+
+                raio = int(math.sqrt((cent_atual[0] - cX1)**2 + (cent_atual[1] - cY1)**2))
+
+                cv2.circle(image, (cent_atual[0], cent_atual[1]), raio, (255, 255, 0), -1)
+
+                cv2.imshow(window_original, image)  # Mostra a imagem de video da webcam
+                cv2.imshow(window_mask, mask_white)  # Mostra a imagem de video da webcam
+                cv2.imshow(window_segmentation, image_processed)  # Mostra a janela com o video segmentado
+
+                key = cv2.waitKey(5)
+
+                # É necessário clicar no 'o' novamente para sair da funcionalidade e gravar na tela branca (canvas)
+                if key == ord('o'):
+                    cv2.circle(mask_white, (cent_atual[0], cent_atual[1]), raio, (255, 255, 0), -1)
+                    # print('You draw a circle with ' + str(raio) + ' pixel radius.')
+                    break
+
+        # Desenha retângulos na tela (canvas)
+        elif key == ord('t'):
+
+            cent_atual = (center_X[len(center_X) - 1], center_Y[len(center_Y) - 1])
+            print(cent_atual)
+
+            while True:
+                _, image = capture.read()  # get an image from the camera
+
+                image = cv2.flip(image, 1)  # espelhar imagem da webcam
+
+                # Criação da imagem processada
+                mins = np.array([ranges['b']['min'], ranges['g']['min'], ranges['r']['min']])
+                max = np.array([ranges['b']['max'], ranges['g']['max'], ranges['r']['max']])
+                image_processed = cv2.inRange(image, mins, max)
+
+                # Criação da máscara para isolar o objeto maior
+                mask_largest = np.ndarray(shape=(height, width), dtype=np.uint8)
+                mask_largest.fill(0)  # Totalmente preta
+
+                if np.mean(image_processed) > 0:  # Verifica se existem objetos (>0)
+
+                    contours, hierarchy = cv2.findContours(image_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+                    largest_item = sorted_contours[0]  # Vai buscar o objeto maior
+                    cv2.fillPoly(mask_largest, pts=[largest_item], color=(255, 255, 255))  # Faz o fill do objeto maior
+
+                    # Calcula as coordenadas do centróide e assinala-o
+                    M = cv2.moments(mask_largest)
+                    cX1 = int(M["m10"] / M["m00"])
+                    cY1 = int(M["m01"] / M["m00"])
+                    # cv2.circle(image, (cX, cY), 5, (0, 0, 255), 2)
+
+                cv2.rectangle(image, (cent_atual[0], cent_atual[1]), (cX1, cY1), (255, 0, 255), -1)
+
+                cv2.imshow(window_original, image)  # Mostra a imagem de video da webcam
+                cv2.imshow(window_mask, mask_white)  # Mostra a imagem de video da webcam
+                cv2.imshow(window_segmentation, image_processed)  # Mostra a janela com o video segmentado
+
+                key = cv2.waitKey(5)
+
+                # É necessário clicar no 'o' novamente para sair da funcionalidade e gravar na tela branca (canvas)
+                if key == ord('t'):
+                    cv2.rectangle(mask_white, (cent_atual[0], cent_atual[1]), (cX1, cY1), (255, 0, 255), -1)
+                    break
+
         #Sai do programa
         elif key == ord('q'):
             print('You pressed q, quitting.')
             break
 
-        if np.mean(image_processed) > 0:        # Verifica se existem objetos (>0)
-
-            count += 1
-
-            contours, hierarchy = cv2.findContours(image_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
-            largest_item = sorted_contours[0]                                                   # Vai buscar o objeto maior
-            cv2.fillPoly(mask_largest, pts=[largest_item], color=(255, 255, 255))                # Faz o fill do objeto maior
-
-            # Calcula as coordenadas do centróide e assinala-o
-            M = cv2.moments(mask_largest)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            # cv2.circle(image, (cX, cY), 5, (0, 0, 255), 2)
-
-            # Desenhar a verde na imagem original
-            mask_largest = mask_largest.astype(np.bool)  # Temos de converter a mask_largest em bool para puder usá-la como 'filtro' na imagem original
-            image[mask_largest] = (0, 255, 0)  # Pintamos de verde na imagem original
-
-            # Desenha uma cruz vermelha no centróide
-            dim_cross = 5
-            cv2.line(image, pt1=(cX-dim_cross, cY-dim_cross), pt2=(cX+dim_cross, cY+dim_cross), color=(0, 0, 255), thickness=2)
-            cv2.line(image, pt1=(cX-dim_cross, cY+dim_cross), pt2=(cX+dim_cross, cY-dim_cross), color=(0, 0, 255), thickness=2)
-
-            #Desenha a linha na máscara branca
-            center_X.append(cX)
-            center_Y.append(cY)
-
-            # print(len(center_X))
-            # print(center_X)
-            # print(center_Y)
-
-            if args.get('shake') == True:
-                if count == 2:
-                    shake_prevention(center_X, center_Y, mask_white, pencil_color, pencil_thickness)
-
-            else:
-                # Verifica se existem argumentos suficientes nos arrays dos centróides para desenhar as linhas
-                if len(center_X) >= 2:
-                    cv2.line(mask_white, pt1=(center_X[count-2], center_Y[count-2]), pt2=(center_X[count-1], center_Y[count-1]), color=pencil_color, thickness=pencil_thickness)
-
-            if count >= 2:
-                del center_X[0]
-                del center_Y[0]
-                count-=1
-
         cv2.imshow(window_original, image)  # Mostra a imagem de video da webcam
         cv2.imshow(window_mask, mask_white)  # Mostra a imagem de video da webcam
         cv2.imshow(window_segmentation, image_processed)  # Mostra a janela com o video segmentado
         cv2.imshow('mask_largest', mask_largest.astype(np.uint8)*255)  # Mostra a imagem de video da webcam, temos de a converter de volta a unit8
-
-
-
 
     cv2.destroyAllWindows()
 
